3aad4ffe3f9e8bcc6762ce75ecda908775a68cab
take-cantik <take.cantik17@gmail.com>
2024-01-11 17:20:36 +0900

    :sparkles: Config provider

---
+++ terraform/staging/terraform.tf
@@ -0,0 +1,11 @@
+# ------------------
+# Terraform
+# ------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ terraform/staging/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type = string
+  default = "north-east-1"
+  description = "AWS region to deploy to"
+}
