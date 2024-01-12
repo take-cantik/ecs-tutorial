a80653184045470bd51dd6833bb78cb83e9b0d47
take-cantik <take.cantik17@gmail.com>
2024-01-12 16:46:24 +0900

    :memo: Add tags

--- terraform/staging/ecr.tf
+++ terraform/staging/ecr.tf
@@ -5,6 +5,10 @@
 # ECR reoisitory for web
 resource "aws_ecr_repository" "web" {
   name = "${var.project_name}-${var.environment}-web"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-web"
+  }
 }
 
 resource "aws_ecr_lifecycle_policy" "web" {
@@ -33,6 +37,10 @@ resource "aws_ecr_lifecycle_policy" "web" {
 # ECR reoisitory for backend
 resource "aws_ecr_repository" "backend" {
   name = "${var.project_name}-${var.environment}-backend"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-backend"
+  }
 }
 
 resource "aws_ecr_lifecycle_policy" "backend" {
@@ -61,6 +69,10 @@ resource "aws_ecr_lifecycle_policy" "backend" {
 # ECR reoisitory for frontend
 resource "aws_ecr_repository" "frontend" {
   name = "${var.project_name}-${var.environment}-frontend"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-frontend"
+  }
 }
 
 resource "aws_ecr_lifecycle_policy" "frontend" {
--- terraform/staging/ecs.tf
+++ terraform/staging/ecs.tf
@@ -1,3 +1,11 @@
+# ------------------
+# ECS
+# ------------------
+
 resource "aws_ecs_cluster" "app" {
   name = "${var.project_name}-${var.environment}-cluster"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-cluster"
+  }
 }
