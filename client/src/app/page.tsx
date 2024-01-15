77033f0059eae42cd650896ff1f79ad65a85972c
take-cantik <take.cantik17@gmail.com>
2024-01-15 15:24:34 +0900

    :zap: Add env for using api url

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,6 @@
 export default async function Home() {
-  const res = await fetch("http://localhost/api/health-check");
+  const apiUrl = process.env.API_URL as string;
+  const res = await fetch(`${apiUrl}/health-check`);
   const text = await res.text();
 
   return <h1>health-check: {text}</h1>;
