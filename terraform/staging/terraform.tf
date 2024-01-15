366a082639e85119c816dd7d901af915b2b811a0
take-cantik <take.cantik17@gmail.com>
2024-01-15 13:21:59 +0900

    :zap: Add tls provider

--- terraform/staging/terraform.tf
+++ terraform/staging/terraform.tf
@@ -7,6 +7,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 
   backend "s3" {
