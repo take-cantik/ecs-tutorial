5d735cd799455e60c9c5d44fe042c4c24e72b2d6
take-cantik <take.cantik17@gmail.com>
2024-01-15 16:45:03 +0900

    :zap: divide fetch function

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,12 @@
-export default async function Home() {
+const getHealthCheck = async () => {
   const apiUrl = process.env.API_URL as string;
   const res = await fetch(`${apiUrl}/health-check`);
   const text = await res.text();
+  return text;
+}
+
+export default async function Home() {
+  const text = await getHealthCheck();
 
   return <h1>health-check: {text}</h1>;
 }
