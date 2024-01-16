31a42c2ae83a2e0917da51fc8fa0d7673e089923
take-cantik <take.cantik17@gmail.com>
2024-01-16 13:26:31 +0900

    :zap: Update target group type to ip from instance

--- terraform/staging/alb.tf
+++ terraform/staging/alb.tf
@@ -67,10 +67,11 @@ resource "aws_lb_listener" "main_http" {
 
 # Target Group
 resource "aws_lb_target_group" "web" {
-  name     = "${var.project_name}-${var.environment}-web"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  name        = "${var.project_name}-${var.environment}-web"
+  target_type = "ip"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
 
   health_check {
     path                = "/api/health-check"
@@ -87,10 +88,11 @@ resource "aws_lb_target_group" "web" {
 }
 
 resource "aws_lb_target_group" "frontend" {
-  name     = "${var.project_name}-${var.environment}-frontend"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  name        = "${var.project_name}-${var.environment}-frontend"
+  target_type = "ip"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
 
   health_check {
     path                = "/"
